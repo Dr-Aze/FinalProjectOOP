e4b5802150aaabdc0dcc550d922760d80281fb32
Aze <hezronkdr@gmail.com>
2026-09-20 20:10:45 +0800

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE attendance(
-    attendanceID INTEGER PRIMARY KEY AUTOINCREMENT,
-    studentID INTEGER NOT NULL,
-    total_class_days INTEGER,
-    days_present INTEGER,
-    total_absences INTEGER,
-);
